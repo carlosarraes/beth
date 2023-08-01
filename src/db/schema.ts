af6415bd772a544440b19aa94b086aadc97cf825
carraes <carraeshb@gmail.com>
2023-08-01 17:38:12 -0300

    chore: update schema to reflect new funcs

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,13 @@
-import { InferModel } from 'drizzle-orm'
+import { InferModel, sql } from 'drizzle-orm'
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
 export const todos = sqliteTable('todos', {
   id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
   content: text('content').notNull(),
   completed: integer('completed', { mode: 'boolean' }).notNull().default(false),
+  price: integer('price', { mode: 'number' }).notNull().default(0),
+  amount: integer('amount', { mode: 'number' }).notNull().default(0),
+  createdAt: text('created_at').default(sql`CURRENT_TIMESTAMP`),
 })
 
 export type Todo = InferModel<typeof todos>
