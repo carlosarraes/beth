e66215f86afe8ffaf8e8938542872bc6435a74e6
carraes <carraeshb@gmail.com>
2023-08-01 17:39:03 -0300

    feat: create header that shows price

---
+++ src/components/header.tsx
@@ -0,0 +1,51 @@
+import elements from 'typed-html'
+
+export const Header = () => {
+  return (
+    <header class="w-full flex justify-between items-center p-2 border-b shadow dark:shadow-gray-400">
+      <div class="flex items-center gap-4">
+        <h1>Smart List -</h1>
+        <div hx-get="/todos/sum" hx-trigger="load" hx-swap="outerHTML" id="show-price"></div>
+      </div>
+      <div class="cursor-pointer" hx-on="click: toggleDarkMode()">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-moon block dark:hidden"
+        >
+          <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-sun-moon hidden dark:block text-white"
+        >
+          <circle cx="12" cy="12" r="4" />
+          <path d="M12 8a2 2 0 1 0 4 4" />
+          <path d="M12 2v2" />
+          <path d="M12 20v2" />
+          <path d="m4.93 4.93 1.41 1.41" />
+          <path d="m17.66 17.66 1.41 1.41" />
+          <path d="M2 12h2" />
+          <path d="M20 12h2" />
+          <path d="m6.34 17.66-1.41 1.41" />
+          <path d="m19.07 4.93-1.41 1.41" />
+        </svg>
+      </div>
+    </header>
+  )
+}
